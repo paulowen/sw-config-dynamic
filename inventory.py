70f9db0a9a72a8b1d5ecbcb353d2671890c075c2
Paul Owen <paulowen@Pauls-MacBook-Pro.local>
2016-10-24 23:08:47 +1100

    Updated field for basic L3 interfaces and SNMP variables amongst others. Updated sample spreadsheet.

--- inventory.py
+++ inventory.py
@@ -12,6 +12,8 @@ import re
 
 from itertools import ifilterfalse
 from collections import defaultdict
+from collections import OrderedDict
+from netaddr import *
 
 class Inventory(object):
 
@@ -21,9 +23,10 @@ class Inventory(object):
         parser.add_argument('--host', action = 'store')
         self.args = parser.parse_args()
 
-    def triggerUpdate(self, host, vlans, switchports):
+    def triggerUpdate(self, host, vlans, switchports, l3ints):
         host.update({"vlans": vlans})
         host.update({"switchports": switchports})
+        host.update({"l3ints": l3ints})
         return host
 
     def Excel2CSV(this, ExcelFile, SheetName, CSVFile):
@@ -68,10 +71,14 @@ class Inventory(object):
     def host_list(self):
 
         # Define list of hosts and skip header
-        meta_dict = {}
-        host_dict = {}
-        vlan_dict = {}
-        port_dict = {}
+        meta_dict = OrderedDict()
+        host_dict = OrderedDict()
+        vlan_dict = OrderedDict()
+        port_dict = OrderedDict()
+        l3_dict = OrderedDict()
+        vlan_index = 100
+        switchport_index = 100
+        l3int_index = 100
 
         self.Excel2CSV('inventory.xlsx', "Sheet1", "inventory.csv")
 
@@ -85,15 +92,19 @@ class Inventory(object):
                 # Trigger update on detection of new device, but only if values exist indicating a host is parsed already
                 if len(host_dict) >> 0:
                     # Update meta dictionary with collated host values, using hostname as key to distinguish for now
-                    meta_dict.update({host: self.triggerUpdate(host_dict, vlan_dict, port_dict)})
+                    meta_dict.update({host: self.triggerUpdate(host_dict, vlan_dict, port_dict, l3_dict)})
 
                 # Set a host variable to remember the last known device
                 host = row["Device"]
 
                 # Reset dictionaries now a new device is detected
-                host_dict = {}
-                vlan_dict = {}
-                port_dict = {}
+                host_dict = OrderedDict()
+                vlan_dict = OrderedDict()
+                port_dict = OrderedDict()
+                l3_dict = OrderedDict()
+                vlan_index = 100
+                switchport_index = 100
+                l3int_index = 100
 
             if len(row["Username"]) >> 0:
                 host_dict.update({'username': row["Username"]})
@@ -104,12 +115,36 @@ class Inventory(object):
             if len(row["Domain Name"]) >> 0:
                 host_dict.update({'domainname': row["Domain Name"]})
 
+            if len(row["Gateway"]) >> 0:
+                host_dict.update({'gateway': row["Gateway"]})
+
+            if len(row["SNMP Location"]) >> 0:
+                host_dict.update({'snmploc': row["SNMP Location"]})
+
             if len(row["VLANs"]) >> 0:
                 if len(row["VLAN Name"]) >> 0:
-                    vlan_dict.update({row["VLANs"]: row["VLAN Name"]})
+                    vlan_dict.update({vlan_index: {'id': row["VLANs"], 'name': row["VLAN Name"]}})
+                    vlan_index += 1
 
-            switchports = {}
+            l3ints = OrderedDict()
+            if len(row["L3 Interfaces"]) >> 0:
+                l3ints.update({'id': row["L3 Interfaces"]})
+
+                if len(row["IP Address"]) >> 0:
+                    ip = IPNetwork(row["IP Address"])
+                    ipaddress = "%s %s" % (ip.ip, ip.netmask)
+                    l3ints.update({'address': ipaddress})
+
+                if len(row["L3 Description"]) >> 0:
+                    l3ints.update({'desc': row["L3 Description"]})
+
+                l3_dict.update({l3int_index: l3ints})
+                l3int_index += 1
+
+            switchports = OrderedDict()
             if len(row["Switchports"]) >> 0:
+                switchports.update({'id': row["Switchports"]})
+
                 if "TRUNK" in row["Mode"]:
                     switchports.update({'mode': 'trunk'})
                     if len(row["Allowed VLANs"]) >> 0:
@@ -128,13 +163,14 @@ class Inventory(object):
                 if "1" in row["Shutdown"]:
                     switchports.update({'shutdown': 'true'})
 
-                port_dict.update({row["Switchports"]: switchports})
+                port_dict.update({switchport_index: switchports})
+                switchport_index += 1
 
         # Update dictionaries again due to end of file - new host detection won't pick it up - still using hostname as key to distinguish
-        meta_dict.update({host: self.triggerUpdate(host_dict, vlan_dict, port_dict)})
+        meta_dict.update({host: self.triggerUpdate(host_dict, vlan_dict, port_dict, l3_dict)})
 
         # Prepare returned dictionary into right format for ansible-playbook using switchname key as distinguisher
-        result = {}
+        result = OrderedDict()
         for key in meta_dict:
             result.update({key: {
                "hosts": [key],
@@ -153,7 +189,7 @@ class Inventory(object):
     def __init__(self):
         self.read_cli_args()
 
-        inventory = {}
+        inventory = OrderedDict()
         # Called with `--list`.
         if self.args.list:
             inventory = self.host_list()
